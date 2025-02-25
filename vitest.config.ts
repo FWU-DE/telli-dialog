85899db487278bb7f6a9c2596bb84e179feb0932
nachthammer <alexandervonberkov1@gmail.com>
2025-02-25 13:21:08 +0100

    test: ignore e2e test folder for vitest

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: ['**/*.{test,spec}.{js,ts,jsx,tsx}'],
+
+    exclude: ['**/node_modules/**', '**/dist/**', '**/e2e/**'],
+
+    environment: 'node',
+
+    testTimeout: 5000,
+
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      exclude: ['**/node_modules/**', '**/dist/**', '**/e2e/**'],
+    },
+  },
+});
