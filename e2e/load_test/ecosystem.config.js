d3c011a6f01535337e32958a2d13367d8168e6d9
nachthammer <alexandervonberkov1@gmail.com>
2025-03-20 17:02:53 +0100

    wip

--- e2e/load_test/ecosystem.config.js
+++ e2e/load_test/ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: 'node',
       args: 'e2e/vidis-mock-server.js',
       instances: 50,
-      exec_mode: 'form',
+      exec_mode: 'fork',
       watch: false,
       wait_ready: true, // Wait for ready signal
       listen_timeout: 10000, // Give it 10 seconds to start up
