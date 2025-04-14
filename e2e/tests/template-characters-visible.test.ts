83ce9ddb8a5af9cbd474c1d97ca90cfb46d83b63
Joshua Ziegler <joshua.ziegler@titanom.com>
2025-04-14 10:11:41 +0200

    add: seed step to dockerfile fix: do not modify feederal state on Conflict remove: .pages format for file upload

---
+++ e2e/tests/template-characters-visible.test.ts
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+import { login } from '../utils/login';
+
+const templateCharactersIdentifier = [
+  'Rosa Parks Civil rights',
+  'George W. Bush 43. Präsident',
+  'Vertretungsstundenplaner (',
+  'Anne Frank Intelligentes jü',
+  'Johann Wolfgang von Goethe',
+  'Frau Goß Schulinterne',
+  'Polizeioberkommissarin Julia',
+];
+
+test('alle predefined characters are visible for everyone', async ({ page }) => {
+  await login(page, 'teacher');
+  await page.goto('/characters?visibility=global');
+
+  await page.waitForURL('/characters**');
+
+  for (const elementIdentifier of templateCharactersIdentifier) {
+    await expect(page.getByRole('link', { name: elementIdentifier })).toBeVisible();
+  }
+});
