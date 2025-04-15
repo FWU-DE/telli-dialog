4c7410bd314f6d2040db736ec89e54aa6ca9e7de
JoshuaZiegler <joshua.ziegler@titanom.com>
2025-04-15 13:38:20 +0200

    Update src/utils/generic/object-operations.ts
    Co-authored-by: Alexander Berkov <alexandervonberkov1@gmail.com>

--- src/utils/generic/object-operations.ts
+++ src/utils/generic/object-operations.ts
@@ -1,6 +1,6 @@
 export function removeNullValues<T extends Record<string, unknown>>(obj: T): Partial<T> {
   return Object.fromEntries(
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    Object.entries(obj).filter(([_, value]) => value !== null && value !== undefined),
+    Object.entries(obj).filter(([, value]) => value !== null && value !== undefined),
   ) as Partial<T>;
 }
