f81626da6f5e1ba7e33b07d69039fadb2a1dae03
Joshua Ziegler <joshua.ziegler@titanom.com>
2025-04-22 15:47:39 +0200

    fix: shared-chat optional fields

---
+++ src/db/migrations/0010_safe_jazinda.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "shared_school_conversation" ALTER COLUMN "specification" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "shared_school_conversation" ALTER COLUMN "restrictions" DROP NOT NULL;
