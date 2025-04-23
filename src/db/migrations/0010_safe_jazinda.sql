0766d4a73ff3f31b2255a08a471710f4bb11256e
Joshua Ziegler <joshua.ziegler@titanom.com>
2025-04-23 15:20:47 +0200

    mod: non default for nullable column

--- src/db/migrations/0010_safe_jazinda.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "shared_school_conversation" ALTER COLUMN "specification" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "shared_school_conversation" ALTER COLUMN "restrictions" DROP NOT NULL;
