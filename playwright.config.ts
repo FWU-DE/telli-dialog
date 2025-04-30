11317218588d97bea9a9cfb8dbcfbf73f88f29ae
Joshua Ziegler <joshua.ziegler@titanom.com>
2025-04-30 13:58:54 +0200

    fix: e2e test due to different input form element labels

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,18 @@ export default defineConfig({
         ...devices['Desktop Chrome'],
       },
     },
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari'],
+      },
+    },
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
+    },
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
