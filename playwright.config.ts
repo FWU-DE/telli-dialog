463fad1b2cc28390e7fb630cfc92579e8ecf10d0
Joshua Ziegler <joshua.ziegler@titanom.com>
2025-04-30 14:29:02 +0200

    removed webkit due to pipeline contraints

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,12 +31,6 @@ export default defineConfig({
       },
     },
     {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-      },
-    },
-    {
       name: 'firefox',
       use: {
         ...devices['Desktop Firefox'],
