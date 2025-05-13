f568a2bd60a29622c0ba1702c1fc05e9c0c74f9a
Joshua Ziegler <joshua.ziegler@titanom.com>
2025-05-13 19:36:55 +0200

    reduce diff

--- src/scripts/mirgrate_otc/index.ts
+++ src/scripts/mirgrate_otc/index.ts
@@ -1,6 +1,6 @@
 import { dumpBucketContent } from './dump_bucket_content';
 
-const ENV_NAME = 'prod';
+const ENV_NAME = 'local';
 
 dumpBucketContent(`./export/${ENV_NAME}`).then(() => {
   console.log(`Bucket content dumped for ${ENV_NAME}`);
