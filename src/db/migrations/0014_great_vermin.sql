291fea41bda3769a3f78655a8e997dfa4c737f51
Joshua Ziegler <joshua.ziegler@titanom.com>
2025-05-15 20:27:36 +0200

    re-apply migration

---
+++ src/db/migrations/0014_great_vermin.sql
@@ -0,0 +1,14 @@
+CREATE EXTENSION IF NOT EXISTS vector;
+
+CREATE TABLE "text_chunk" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"file_id" text NOT NULL,
+	"embedding" vector(1024) NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"content" text NOT NULL,
+	"order_index" integer NOT NULL,
+	"page_number" integer
+);
+--> statement-breakpoint
+ALTER TABLE "shared_school_conversation" ADD COLUMN "restrictions" text;--> statement-breakpoint
+ALTER TABLE "text_chunk" ADD CONSTRAINT "text_chunk_file_id_file_table_id_fk" FOREIGN KEY ("file_id") REFERENCES "public"."file_table"("id") ON DELETE no action ON UPDATE no action;
