2b7bceba9605d9cd6b2066ba4df51246d38186b1
JoshuaZiegler <joshua.ziegler@titanom.com>
2025-06-06 10:12:59 +0200

    fix: configuration for mandatory form fields
    remove: min requirement as it interferres with the required property (#89)

--- src/app/(authed)/(dialog)/custom/editor/[customgptId]/custom-gpt-form.tsx
+++ src/app/(authed)/(dialog)/custom/editor/[customgptId]/custom-gpt-form.tsx
@@ -48,9 +48,9 @@ type CustomGptFormProps = CustomGptModel & {
 };
 
 const customGptFormValuesSchema = z.object({
-  name: z.string().min(1),
-  description: z.string().min(1).max(TEXT_INPUT_FIELDS_LENGTH_LIMIT),
-  specification: z.string().min(1).max(TEXT_INPUT_FIELDS_LENGTH_LIMIT),
+  name: z.string(),
+  description: z.string().max(TEXT_INPUT_FIELDS_LENGTH_LIMIT),
+  specification: z.string().max(TEXT_INPUT_FIELDS_LENGTH_LIMIT),
   promptSuggestions: z.array(z.object({ content: z.string() })),
 });
 type CustomGptFormValues = z.infer<typeof customGptFormValuesSchema>;
