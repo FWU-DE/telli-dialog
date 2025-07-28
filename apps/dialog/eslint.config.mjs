c32910893f415dd045b5c83af3d5392e72af9e22
robin.laerm <robin.laerm@xitaso.com>
2025-07-28 10:52:01 +0200

    TD-350 adapt prettier, eslint and turbo config

--- apps/dialog/eslint.config.mjs
+++ apps/dialog/eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
+import { off } from 'process';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,8 +12,10 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ['next', 'next/core-web-vitals', 'next/typescript'],
+    extends: ['next', 'next/core-web-vitals', 'next/typescript', 'prettier'],
+    plugins: ['prettier'],
     rules: {
+      'prettier/prettier': 'error',
       'no-restricted-imports': [
         'error',
         {
