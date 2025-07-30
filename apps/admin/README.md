24668268318a32b4a0aedb31c1d791c968a31a6c
robin.laerm <robin.laerm@xitaso.com>
2025-07-30 12:13:24 +0200

    TD-364 first version of telli-admin

---
+++ apps/admin/README.md
@@ -0,0 +1,5 @@
+# Admin App
+
+This is the admin application for the telli-dialog monorepo.
+It is a Next.js app with TypeScript, set up similarly to the dialog app.
+It enables admins to configure telli-api and telli-dialog.
---
+++ apps/admin/eslint.config.mjs
@@ -0,0 +1,19 @@
+// https://nextjs.org/docs/app/api-reference/config/eslint#specifying-a-root-directory-within-a-monorepo
+import { FlatCompat } from '@eslint/eslintrc';
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next', 'text/typescript', 'prettier'],
+    settings: {
+      next: {
+        rootDir: 'packages/my-app/',
+      },
+    },
+  }),
+];
+
+export default eslintConfig;
