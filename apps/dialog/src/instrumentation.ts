196ddcbe8a848514f80929e963fd59b02aca158d
robin.laerm <robin.laerm@xitaso.com>
2025-08-13 17:08:34 +0200

    TD-388 enable traces for telli-admin and telli-dialog

--- apps/dialog/src/instrumentation.ts
+++ apps/dialog/src/instrumentation.ts
@@ -2,10 +2,17 @@ import * as Sentry from '@sentry/nextjs';
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
+    console.info(
+      `NEXT_RUNTIME is ${process.env.NEXT_RUNTIME} - registering nodejs instrumentation.`,
+    );
+    await import('./instrumentation.node');
     await import('../sentry.server.config');
   }
 
   if (process.env.NEXT_RUNTIME === 'edge') {
+    console.info(
+      `NEXT_RUNTIME is ${process.env.NEXT_RUNTIME} - no instrumentation available for edge.`,
+    );
     await import('../sentry.edge.config');
   }
 }
