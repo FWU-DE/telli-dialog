0a3d77ec4591a1fc0a77de9061fa67fd63323c3a
robin.laerm <robin.laerm@xitaso.com>
2025-09-16 16:32:40 +0200

    TD-424 move loading code and types into different folders

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,26 +1,7 @@
 import Link from 'next/link';
-import { env } from '../consts/env';
-import { routes } from '../consts/routes';
-import { FederalState } from '../types/federal-state';
-import Loading from './loading';
+import { fetchFederalStates } from '../services/federal-states';
 
 export default async function Home() {
-  async function fetchFederalStates() {
-    const response = await fetch(env.BASE_URL_TELLI_DIALOG + routes.FEDERAL_STATES_ROUTE, {
-      mode: 'no-cors',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${env.API_KEY_TELLI_DIALOG}`,
-      },
-    });
-    if (!response.ok) {
-      throw new Error(`Failed to fetch federal states: ${response.statusText}`);
-    }
-
-    const data = await response.json();
-    // validate response with zod
-    return data.federalStates as FederalState[];
-  }
   const federalStates = await fetchFederalStates();
 
   return (
