9da10c3d8878c4da923bba9cd9d3f3d3deaaa201
Maximilian Asam <maximilian.asam@xitaso.com>
2025-09-22 12:25:57 +0200

    fix format

--- apps/dialog/e2e/tests/character/create-character-from-template.test.ts
+++ apps/dialog/e2e/tests/character/create-character-from-template.test.ts
@@ -4,10 +4,8 @@ import { login } from '../../utils/login';
 test('create character from template', async ({ page }) => {
   await login(page, 'teacher');
   await page.goto('/characters?visibility=global');
-  const link = page
-    .getByRole('link', { name: 'Johann Wolfgang von Goethe' })
-  const copyButton = link
-    .getByRole('button', { name: 'Kopieren' });
+  const link = page.getByRole('link', { name: 'Johann Wolfgang von Goethe' });
+  const copyButton = link.getByRole('button', { name: 'Kopieren' });
 
   expect(link).toBeVisible();
   expect(copyButton).toBeVisible();
