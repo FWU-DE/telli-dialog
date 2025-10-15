484251bffc94e97ffa620408dea58e661465ad71
Tobias Wallura <tobias.wallura@xitaso.com>
2025-10-15 17:53:58 +0200

    TD-470 create span for sending RabbitMq event

--- apps/dialog/src/rabbitmq/send.ts
+++ apps/dialog/src/rabbitmq/send.ts
@@ -1,3 +1,4 @@
+import { trace } from '@opentelemetry/api';
 import { Connection } from 'rabbitmq-client';
 
 const rabbitmqUri = process.env['RABBITMQ_URI'];
@@ -6,11 +7,16 @@ if (rabbitmqUri === undefined) {
   throw Error('Expected process.env.RABBITMQ_URI to be defined');
 }
 
+const tracer = trace.getTracer('rabbitmq-client');
+
 const rabbit = new Connection(rabbitmqUri);
 
 const publisher = rabbit.createPublisher({ confirm: true });
 
 export async function sendRabbitmqEvent(event: object) {
-  console.info('Sending event...', event);
-  await publisher.send({ routingKey: 'events', exchange: '' }, event);
+  await tracer.startActiveSpan('sendRabbitmqEvent', async (span) => {
+    console.info('Sending event...', event);
+    await publisher.send({ routingKey: 'events', exchange: '' }, event);
+    span.end();
+  });
 }
