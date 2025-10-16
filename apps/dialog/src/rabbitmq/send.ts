a0543ac9c5126e390960458831ba36892b042736
Tobias Wallura <161311015+TobiasWallura-xitaso@users.noreply.github.com>
2025-10-16 14:00:09 +0200

    TD-470 publish rabbitmq event in background and log errors (#210)

--- apps/dialog/src/rabbitmq/send.ts
+++ apps/dialog/src/rabbitmq/send.ts
@@ -1,5 +1,6 @@
 import { trace } from '@opentelemetry/api';
 import { Connection } from 'rabbitmq-client';
+import { logError } from '@/utils/logging/logging';
 
 const rabbitmqUri = process.env['RABBITMQ_URI'];
 
@@ -14,9 +15,14 @@ const rabbit = new Connection(rabbitmqUri);
 const publisher = rabbit.createPublisher({ confirm: true });
 
 export async function sendRabbitmqEvent(event: object) {
-  await tracer.startActiveSpan('sendRabbitmqEvent', async (span) => {
+  // Do not await published event for performance reasons
+  void tracer.startActiveSpan('sendRabbitmqEvent', async (span) => {
     console.info('Sending event...', event);
-    await publisher.send({ routingKey: 'events', exchange: '' }, event);
+    try {
+      await publisher.send({ routingKey: 'events', exchange: '' }, event);
+    } catch (error) {
+      logError('Sending event to RabbitMq failed', error);
+    }
     span.end();
   });
 }
