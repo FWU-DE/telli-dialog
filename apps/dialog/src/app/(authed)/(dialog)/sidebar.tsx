b88e4d5809841cef1ce4d666efb26415037995e2
robin.laerm <robin.laerm@xitaso.com>
2025-10-21 11:53:17 +0200

    fix null checks for telliName

--- apps/dialog/src/app/(authed)/(dialog)/sidebar.tsx
+++ apps/dialog/src/app/(authed)/(dialog)/sidebar.tsx
@@ -123,7 +123,7 @@ export default function DialogSidebar({ user, currentModelCosts, userPriceLimit 
           <Link href="/" className="w-full flex gap-2 items-center hover:underline px-2 py-1.5">
             <TelliIcon className="w-4 h-4 fill-primary" />
             <span className="text-base font-medium text-primary">
-              {user.federalState.telliName ?? 'telli'}
+              {!!user.federalState.telliName ? user.federalState.telliName : 'telli'}
             </span>
           </Link>
           <Link href="/custom" className="w-full mt-1">
