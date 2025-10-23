b7ad72dc8f52fa55710c98bdb266833de1cab2a1
Tobias Wallura <161311015+TobiasWallura-xitaso@users.noreply.github.com>
2025-10-23 11:39:50 +0200

    TD-537 setup auto-instrumentations-node and update otel dependencies (#219)
    * TD-537 setup auto-instrumentations-node and update otel dependencies
    
    * TD-537 include app version as attribute in traces
    
    * TD-537 update dependency require-in-the-middle to fix version conflicts

--- apps/admin/src/instrumentation.node.ts
+++ apps/admin/src/instrumentation.node.ts
@@ -1,10 +1,11 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { resourceFromAttributes } from '@opentelemetry/resources';
-import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
+import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from '@opentelemetry/semantic-conventions';
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-node';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 
 // For debugging purposes, you can uncomment the following two lines to enable console logging
 // import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
@@ -22,8 +23,11 @@ const periodicExportingMetricReader = new PeriodicExportingMetricReader({
 // Documentation for the OpenTelemetry SDK for Node.js can be found here:
 // https://www.npmjs.com/package/@opentelemetry/sdk-node
 const sdk = new NodeSDK({
-  autoDetectResources: true,
-  resource: resourceFromAttributes({ [ATTR_SERVICE_NAME]: SERVICE_NAME }),
+  instrumentations: [getNodeAutoInstrumentations()],
+  resource: resourceFromAttributes({
+    [ATTR_SERVICE_NAME]: SERVICE_NAME,
+    [ATTR_SERVICE_VERSION]: process.env.APP_VERSION,
+  }),
   metricReader: periodicExportingMetricReader,
   serviceName: SERVICE_NAME,
   spanProcessors: [new BatchSpanProcessor(new OTLPTraceExporter())],
