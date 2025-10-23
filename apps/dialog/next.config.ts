40cd0a1b6505e6b6c72c3e9951aa4f0a3aa95a85
Tobias Wallura <161311015+TobiasWallura-xitaso@users.noreply.github.com>
2025-10-23 20:04:55 +0200

    Enable Sentry browserTracingIntegration (#204)

--- apps/dialog/next.config.ts
+++ apps/dialog/next.config.ts
@@ -42,8 +42,7 @@ const baseNextConfig: NextConfig = {
   experimental: {
     useCache: true,
   },
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  webpack: (config, { isServer }) => {
+  webpack: (config) => {
     // Ensure proper module resolution for path aliases
     config.resolve.alias = {
       ...config.resolve.alias,
@@ -89,11 +88,6 @@ export default withSentryConfig(baseNextConfigWithNextIntl, {
     enabled: true,
   },
 
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  tunnelRoute: '/monitoring',
   // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
 });
