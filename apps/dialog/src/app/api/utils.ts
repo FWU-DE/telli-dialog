186b244f7f31ad1c8590affe032925b115829e95
Elena Jeron <elena.jeron@fwu.de>
2025-10-26 06:45:19 +0100

    set fallback of auxiliary model, fix fallback of selected model

--- apps/dialog/src/app/api/utils.ts
+++ apps/dialog/src/app/api/utils.ts
@@ -97,7 +97,6 @@ export async function getAuxiliaryModel(federalStateId: string): Promise<LlmMode
   const auxiliaryModel =
     getDefaultAuxModel(llmModels) ??
     getFallbackAuxModel(llmModels) ??
-    getFirstEmbeddingModel(llmModels) ??
     getFirstTextModel(llmModels);
   if (auxiliaryModel === undefined) {
     throw new Error('No auxiliary model found');
@@ -113,10 +112,6 @@ function getFallbackAuxModel(models: LlmModel[]): LlmModel | undefined {
   return models.find((model) => model.name === FALLBACK_AUXILIARY_MODEL);
 }
 
-function getFirstEmbeddingModel(models: LlmModel[]): LlmModel | undefined {
-  return models.find((model) => model.priceMetadata.type === 'embedding');
-}
-
-function getFirstTextModel(models: LlmModel[]): LlmModel | undefined {
+export function getFirstTextModel(models: LlmModel[]): LlmModel | undefined {
   return models.find((model) => model.priceMetadata.type === 'text');
 }
