2cbec8821a05034eaf74a3b00b11eb21a306f866
Tobias Wallura <tobias.wallura@xitaso.com>
2025-10-27 21:03:48 +0100

    TD-396 fix warning in logs for loading.gif

--- apps/dialog/src/components/chat/loading-animation.tsx
+++ apps/dialog/src/components/chat/loading-animation.tsx
@@ -13,7 +13,7 @@ export default function LoadingAnimation({
 
   return (
     <div className="text-secondary-foreground w-fit m-4">
-      <Image src="/loading.gif" alt="Ladeanimation" width="107" height="107" />
+      <Image src="/loading.gif" alt="Ladeanimation" width="107" height="107" unoptimized />
       <span>{message}</span>
     </div>
   );
