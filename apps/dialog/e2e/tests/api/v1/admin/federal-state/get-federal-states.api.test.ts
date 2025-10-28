07a7b5e05eadb274b0a419864c0adc1219bd8f9b
Maximilian Asam <asam.maximilian@gmail.com>
2025-10-28 13:35:36 +0100

    TD-530: Fix Tests (#228)
    * fix api test
    
    * remove test for decrypted api key
    
    * Rework seeding, so requirements are actually there
    
    * fix format
    
    * give more time for flaky test
    
    * format
    
    * logging fix

--- apps/dialog/e2e/tests/api/v1/admin/federal-state/get-federal-states.api.test.ts
+++ apps/dialog/e2e/tests/api/v1/admin/federal-state/get-federal-states.api.test.ts
@@ -3,20 +3,13 @@ import { authorizationHeader } from '../../../../../utils/authorizationHeader';
 
 const federalStateRoute = '/api/v1/admin/federal-states';
 
-test('should fetch all federal states with decryptedApiKey', async ({
-  request,
-}: {
-  request: APIRequestContext;
-}) => {
+test('should fetch all federal states', async ({ request }: { request: APIRequestContext }) => {
   const response = await request.get(federalStateRoute, {
     headers: { ...authorizationHeader },
   });
   expect(response.ok()).toBeTruthy();
   const json = await response.json();
   expect(Array.isArray(json.federalStates)).toBe(true);
-  if (json.federalStates.length > 0) {
-    expect(json.federalStates[0]).toHaveProperty('decryptedApiKey');
-  }
 });
 
 test('should return 403 if authorization header is missing', async ({
