fd7752d5fc784e97f5b3995046b0b29099a933eb
robin.laerm <robin.laerm@xitaso.com>
2025-10-31 16:44:50 +0100

    try to fix the type error

--- apps/dialog/src/utils/chat/messages.ts
+++ apps/dialog/src/utils/chat/messages.ts
@@ -4,13 +4,16 @@ import { Message } from 'ai';
 export function convertMessageModelToMessage(
   messages: Array<ConversationMessageModel>,
 ): Array<Message> {
-  const test = messages
+  return messages
     .filter((message) => message.role !== 'tool')
     .map((message) => ({
       id: message.id,
-      role: message.role,
+      // Todo Anton: Please check the typing here
+      // I got an error that 'tool' is not assignable to type 'system' | 'user' | 'assistant' | 'data'
+      // Regarding documentation of ai package in version 4, 'tool' is valid
+      // Version of ai packages did not change but after moving code to shared package this error appeared
+      role: message.role as Exclude<ConversationMessageModel['role'], 'tool'>,
       content: message.content,
       createdAt: message.createdAt,
     }));
-  return test;
 }
