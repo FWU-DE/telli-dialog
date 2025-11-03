ac1995678ea744d7755b139fadf31cb06ec8226d
Tobias Wallura <tobias.wallura@xitaso.com>
2025-11-03 19:33:23 +0100

    TD-590 ensure db migrations don't run concurrently and are executed only once

--- packages/shared/src/db/index.ts
+++ packages/shared/src/db/index.ts
@@ -1,15 +1,16 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { migrate } from 'drizzle-orm/node-postgres/migrator';
 import { Pool } from 'pg';
 import path from 'node:path';
 import { env } from '../env';
+import { migrateWithLock } from './migrate';
 
-const globalPool = global as unknown as {
+const globalState = global as unknown as {
   pool?: Pool;
+  migrationsRun?: boolean;
 };
 
 const pool =
-  globalPool.pool ??
+  globalState.pool ??
   new Pool({
     connectionString: env.databaseUrl,
     max: 12,
@@ -17,21 +18,27 @@ const pool =
 
 // In development mode, store the pool globally to reuse it across hot reloads.
 if (process.env.NODE_ENV === 'development') {
-  globalPool.pool = pool;
+  globalState.pool = pool;
 }
 const db = drizzle({ client: pool });
 
-// cannot use `await` at the top level in a module, so we use an IIFE
-(async () => {
-  try {
-    console.info('Running database migrations...');
-    await migrate(db, {
-      migrationsFolder: path.join(process.cwd(), '..', '..', 'packages', 'shared', 'migrations'),
-    });
-    console.info('Database migrations completed successfully.');
-  } catch (error) {
-    console.error('Error running database migrations:', error);
-  }
-})();
+// Ensure the migrations are executed only once
+// Note: migrations won't be run on hot reload
+if (!globalState.migrationsRun) {
+  globalState.migrationsRun = true;
+
+  // cannot use `await` at the top level in a module, so we use an IIFE
+  (async () => {
+    try {
+      console.info('Running database migrations...');
+      await migrateWithLock(db, {
+        migrationsFolder: path.join(process.cwd(), '..', '..', 'packages', 'shared', 'migrations'),
+      });
+      console.info('Database migrations completed successfully.');
+    } catch (error) {
+      console.error('Error running database migrations:', error);
+    }
+  })();
+}
 
 export { db };
