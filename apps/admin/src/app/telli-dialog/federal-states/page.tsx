b1c073649f31e2a17cae51f34025c481db8ccfa9
rlaerm <Robin.Laerm@xitaso.com>
2025-11-04 12:08:27 +0100

    Feat/td 576 support contacts (#252)
    * TD-581 support contacts is just text now
    
    * move navigation to page component

--- apps/admin/src/app/telli-dialog/federal-states/page.tsx
+++ apps/admin/src/app/telli-dialog/federal-states/page.tsx
@@ -1,7 +1,20 @@
+import TwoColumnLayout from '@/components/layout/TwoColumnLayout';
 import FederalStateListView from './FederalStateListView';
+import { Sidebar, SidebarItem } from '@/components/navigation/Sidebar';
+import { ROUTES } from '@/consts/routes';
 
 export const dynamic = 'force-dynamic';
 
 export default function FederalStatesPage() {
-  return <FederalStateListView />;
+  return (
+    <TwoColumnLayout
+      sidebar={
+        <Sidebar>
+          <SidebarItem label="Bundesländer" href={ROUTES.dialog.federalStates} />
+          <SidebarItem label="Vorlagen" href={ROUTES.dialog.templates} />
+        </Sidebar>
+      }
+      page={<FederalStateListView />}
+    />
+  );
 }
