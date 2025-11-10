db451965df14f5fd1f1009399fa87093de9beef7
Tobias Wallura <tobias.wallura@xitaso.com>
2025-11-10 20:06:16 +0100

    TD-613 remove unused code

--- packages/shared/src/db/crypto.ts
+++ packages/shared/src/db/crypto.ts
@@ -1,15 +1,4 @@
-import bcrypt from 'bcrypt';
 import crypto from 'crypto';
-
-export async function hashPassword(password: string, saltRounds: number = 10): Promise<string> {
-  const salt = await bcrypt.genSalt(saltRounds);
-  const hashedPassword = await bcrypt.hash(password, salt);
-  return hashedPassword;
-}
-
-export async function verifyPassword(password: string, hash: string): Promise<boolean> {
-  return await bcrypt.compare(password, hash);
-}
 
 const ALGORITHM = 'aes-256-cbc';
 
