8431514f5f1ad8518240b75ade254ce1fb89a59f
robin.laerm <robin.laerm@xitaso.com>
2025-11-14 12:54:52 +0100

    federal-state-provider and check for feature toggle

---
+++ apps/dialog/src/components/providers/federal-state-provider.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import React from 'react';
+import { FederalStateModel } from '@shared/types/federal-state';
+
+const FederalStateContext = React.createContext<FederalStateModel | undefined>(undefined);
+
+export function useFederalState(): FederalStateModel | undefined {
+  return React.useContext(FederalStateContext);
+}
+
+export function FederalStateProvider({
+  children,
+  federalState,
+}: {
+  children: React.ReactNode;
+  federalState: FederalStateModel;
+}) {
+  return <FederalStateContext value={federalState}>{children}</FederalStateContext>;
+}
