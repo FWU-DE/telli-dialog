de197a4c75d3a5c9c3616ece07fac54dc838cc98
robin.laerm <robin.laerm@xitaso.com>
2025-11-17 12:04:37 +0100

    fix sql migration

--- packages/shared/migrations/0032_tranquil_blackheart.sql
+++ packages/shared/migrations/0032_tranquil_blackheart.sql
@@ -1,14 +1,14 @@
-- ALTER TABLE "federal_state" ADD COLUMN "feature_toggles" json;--> statement-breakpoint
+ALTER TABLE "federal_state" ADD COLUMN "feature_toggles" json;--> statement-breakpoint
   -- migrate data from old columns to new json column  
-- UPDATE "federal_state" SET "feature_toggles" = json_build_object(
-- 'isStudentAccessEnabled', "student_access",
-- 'isCharacterEnabled', "enable_characters",
-- 'isSharedChatEnabled', "enable_shared_chats",
-- 'isCustomGptEnabled', "enable_custom_gpts",
-- 'isShareTemplateWithSchoolEnabled', true
-- );--> statement-breakpoint
-- ALTER TABLE "federal_state" ALTER COLUMN "feature_toggles" SET NOT NULL;--> statement-breakpoint
-- ALTER TABLE "federal_state" DROP COLUMN "student_access";--> statement-breakpoint
-- ALTER TABLE "federal_state" DROP COLUMN "enable_characters";--> statement-breakpoint
-- ALTER TABLE "federal_state" DROP COLUMN "enable_shared_chats";--> statement-breakpoint
-- ALTER TABLE "federal_state" DROP COLUMN "enable_custom_gpts";
+UPDATE "federal_state" SET "feature_toggles" = json_build_object(
+'isStudentAccessEnabled', "student_access",
+'isCharacterEnabled', "enable_characters",
+'isSharedChatEnabled', "enable_shared_chats",
+'isCustomGptEnabled', "enable_custom_gpts",
+'isShareTemplateWithSchoolEnabled', true
+);--> statement-breakpoint
+ALTER TABLE "federal_state" ALTER COLUMN "feature_toggles" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "federal_state" DROP COLUMN "student_access";--> statement-breakpoint
+ALTER TABLE "federal_state" DROP COLUMN "enable_characters";--> statement-breakpoint
+ALTER TABLE "federal_state" DROP COLUMN "enable_shared_chats";--> statement-breakpoint
+ALTER TABLE "federal_state" DROP COLUMN "enable_custom_gpts";
