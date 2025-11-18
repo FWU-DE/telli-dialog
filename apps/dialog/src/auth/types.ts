4afb29e3a06d454bba667a4fa32cf9fe13497bb0
robin.laerm <robin.laerm@xitaso.com>
2025-11-18 11:30:23 +0100

    use named import

--- apps/dialog/src/auth/types.ts
+++ apps/dialog/src/auth/types.ts
@@ -4,7 +4,7 @@ import {
   userSchoolRoleSchema,
   userSelectSchema,
 } from '@shared/db/schema';
-import z from 'zod';
+import { z } from 'zod';
 
 const obscuredFederalStateSchema = federalStateSelectSchema.omit({ encryptedApiKey: true });
 const userSchoolSchema = schoolSelectSchema.extend({
