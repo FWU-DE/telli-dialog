a07354e296431b0635e6ce5c0990203ce81dfd3d
robin.laerm <robin.laerm@xitaso.com>
2025-11-18 15:08:41 +0100

    add withAuth wrapper

---
+++ apps/dialog/src/auth/withAuth.ts
@@ -0,0 +1,23 @@
+// lib/withAuth.ts
+'use server';
+
+import { auth } from '@/auth';
+import { Session } from 'next-auth';
+
+/**
+ * Wrap a server action with authentication.
+ * The wrapped function receives the Session as the first argument.
+ */
+export function withAuth<Args extends any[], Return>(
+  fn: (session: Session, ...args: Args) => Promise<Return>,
+) {
+  return async (...args: Args): Promise<Return> => {
+    const session = await auth();
+
+    if (!session) {
+      throw new Error('Unauthorized');
+    }
+
+    return fn(session, ...args);
+  };
+}
