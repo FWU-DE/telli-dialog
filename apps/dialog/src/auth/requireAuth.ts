e3f6f5be832163ea567af698bd9490debcc8d398
robin.laerm <robin.laerm@xitaso.com>
2025-11-19 10:20:17 +0100

    add requireAuth function

--- apps/dialog/src/auth/requireAuth.ts
+++ apps/dialog/src/auth/requireAuth.ts
@@ -1,7 +1,24 @@
 import { auth } from '@/auth';
+import {
+  federalStateSelectSchema,
+  SchoolModel,
+  schoolSelectSchema,
+  UserModel,
+  userSchema,
+} from '@shared/db/schema';
+import { ObscuredFederalState } from './utils';
+import { UnauthorizedError } from '@shared/error';
 
-export async function requireAuth() {
+export async function requireAuth(): Promise<{
+  user: UserModel;
+  school: SchoolModel;
+  federalState: ObscuredFederalState;
+}> {
   const session = await auth();
-  if (!session) throw new Error('Unauthorized');
-  return session;
+  if (!session) throw new UnauthorizedError();
+
+  const user = userSchema.parse(session.user);
+  const school = schoolSelectSchema.parse(session.user?.school);
+  const federalState = federalStateSelectSchema.parse(session.user?.federalState);
+  return { user: user, school: school, federalState: federalState };
 }
