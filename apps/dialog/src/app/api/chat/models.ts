1009cbea4ff5ad70f3af2954a74540cae420913a
Elena Jeron <elena.jeron@fwu.de>
2025-11-25 11:33:15 +0100

    set default to gpt

--- apps/dialog/src/app/api/chat/models.ts
+++ apps/dialog/src/app/api/chat/models.ts
@@ -1,6 +1,6 @@
-export const DEFAULT_CHAT_MODEL: string = 'gpt-5-mini';
+export const DEFAULT_CHAT_MODEL: string = 'gpt-5-nano';
 /* Auxilary tasks are chat history condensation and keyword extraction
 which are resource intensive and should be done with a less powerful model
 */
-export const DEFAULT_AUXILIARY_MODEL: string = 'gpt-4o-mini';
+export const DEFAULT_AUXILIARY_MODEL: string = 'gpt-5-nano';
 export const FALLBACK_AUXILIARY_MODEL: string = 'meta-llama/Llama-3.3-70B-Instruct';
