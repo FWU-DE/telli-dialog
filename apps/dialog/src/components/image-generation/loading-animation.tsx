41cc56ae9ed4126024f3ab70218b42b9098f8e02
Maximilian Asam <maximilian.asam@xitaso.com>
2025-12-04 15:32:55 +0100

    Replace with temporary spinner, while gif is being updated

--- apps/dialog/src/components/image-generation/loading-animation.tsx
+++ apps/dialog/src/components/image-generation/loading-animation.tsx
@@ -16,7 +16,9 @@ export default function LoadingAnimation({
     <div
       className={`flex flex-col items-center justify-center aspect-square border-2 border-none bg-gray-50 rounded-enterprise-md mb-4 ${className}`}
     >
-      <Image src="/loading.gif" alt="Ladeanimation" width="107" height="107" unoptimized />
+      {/* TODO: When Gif is supplied, replace with the following line */}
+      {/* <Image src="/loading.gif" alt="Ladeanimation" width="107" height="107" unoptimized /> */}
+      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-primary mb-4"></div>
       <span className="text-gray-900 text-sm">{message}</span>
     </div>
   );
