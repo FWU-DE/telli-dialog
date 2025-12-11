d2d17e074815291329816df73b991e7518a8904b
Tobias Wallura <tobias.wallura@xitaso.com>
2025-12-11 13:51:35 +0100

    TD-784 add api tests for cleanup endpoint

---
+++ apps/dialog/e2e/tests/api/v1/admin/cleanup/cleanup.api.test.ts
@@ -0,0 +1,226 @@
+import test, { expect } from '@playwright/test';
+import { authorizationHeader } from '../../../../../utils/authorizationHeader';
+import { db } from '@shared/db';
+import {
+  CharacterFileMapping,
+  characterTable,
+  CustomGptFileMapping,
+  customGptTable,
+  fileTable,
+  llmModelTable,
+  SharedSchoolConversationFileMapping,
+  SharedSchoolConversationInsertModel,
+  sharedSchoolConversationTable,
+  userTable,
+} from '@shared/db/schema';
+import { eq } from 'drizzle-orm';
+import { generateUUID } from '@shared/utils/uuid';
+import { createInsertSchema } from 'drizzle-zod';
+import { z } from 'zod';
+
+const cleanupRoute = '/api/v1/admin/cleanup';
+
+test.describe('cleanup', () => {
+  let userId = '';
+  let modelId = '';
+
+  test.beforeEach(async () => {
+    await db.transaction(async (tx) => {
+      const [user] = await tx
+        .insert(userTable)
+        .values({ firstName: '', lastName: '', email: generateUUID() })
+        .returning();
+      if (!user) {
+        throw Error('Failed to create user');
+      }
+      userId = user.id;
+
+      const [model] = await tx.select().from(llmModelTable).limit(1);
+      if (!model) {
+        throw Error('Failed to find model');
+      }
+      modelId = model.id;
+    });
+  });
+
+  test.afterEach(async () => {
+    await db.transaction(async (tx) => {
+      await tx
+        .delete(sharedSchoolConversationTable)
+        .where(eq(sharedSchoolConversationTable.userId, userId));
+      await tx.delete(characterTable).where(eq(characterTable.userId, userId));
+      await tx.delete(customGptTable).where(eq(customGptTable.userId, userId));
+      await tx.delete(userTable).where(eq(userTable.id, userId));
+    });
+  });
+
+  test('should delete learning scenarios', async ({ request }) => {
+    const oldLearningScenario = await createLearningScenario({
+      userId,
+      modelId,
+      createdAt: new Date(2025, 0, 1),
+    });
+    const newLearningScenario = await createLearningScenario({
+      userId,
+      modelId,
+      createdAt: new Date(),
+    });
+
+    // Delete
+    const response = await request.delete(cleanupRoute, { headers: authorizationHeader });
+
+    expect(response.ok()).toBeTruthy();
+    const json = await response.json();
+    expect(json.deletedLearningScenarios.length).toBeGreaterThanOrEqual(1);
+
+    const resultDeleted = await db
+      .select()
+      .from(sharedSchoolConversationTable)
+      .where(eq(sharedSchoolConversationTable.id, oldLearningScenario.id));
+    expect(resultDeleted).toHaveLength(0);
+
+    const resultExisting = await db
+      .select()
+      .from(sharedSchoolConversationTable)
+      .where(eq(sharedSchoolConversationTable.id, newLearningScenario.id));
+    expect(resultExisting).toHaveLength(1);
+  });
+
+  test('should delete characters', async ({ request }) => {
+    const oldCharacter = await createCharacter({
+      userId,
+      modelId,
+      createdAt: new Date(2025, 0, 1),
+    });
+    const newCharacter = await createCharacter({
+      userId,
+      modelId,
+      createdAt: new Date(),
+    });
+
+    // Delete
+    const response = await request.delete(cleanupRoute, { headers: authorizationHeader });
+
+    expect(response.ok()).toBeTruthy();
+    const json = await response.json();
+    expect(json.deletedCharacters.length).toBeGreaterThanOrEqual(1);
+
+    const resultDeleted = await db
+      .select()
+      .from(characterTable)
+      .where(eq(characterTable.id, oldCharacter.id));
+    expect(resultDeleted).toHaveLength(0);
+
+    const resultExisting = await db
+      .select()
+      .from(characterTable)
+      .where(eq(characterTable.id, newCharacter.id));
+    expect(resultExisting).toHaveLength(1);
+  });
+
+  test('should delete custom GPTs', async ({ request }) => {
+    const oldCustomGpt = await createCustomGpt({
+      userId,
+      createdAt: new Date(2025, 0, 1),
+    });
+    const newCustomGpt = await createCustomGpt({
+      userId,
+      createdAt: new Date(),
+    });
+
+    // Delete
+    const response = await request.delete(cleanupRoute, { headers: authorizationHeader });
+
+    expect(response.ok()).toBeTruthy();
+    const json = await response.json();
+    expect(json.deletedCustomGpts.length).toBeGreaterThanOrEqual(1);
+
+    const resultDeleted = await db
+      .select()
+      .from(customGptTable)
+      .where(eq(customGptTable.id, oldCustomGpt.id));
+    expect(resultDeleted).toHaveLength(0);
+
+    const resultExisting = await db
+      .select()
+      .from(customGptTable)
+      .where(eq(customGptTable.id, newCustomGpt.id));
+    expect(resultExisting).toHaveLength(1);
+  });
+});
+
+test('should return 403 if authorization header is missing', async ({ request }) => {
+  const response = await request.delete(cleanupRoute);
+  expect(response.status()).toBe(403);
+});
+
+async function createLearningScenario(data?: Partial<SharedSchoolConversationInsertModel>) {
+  const [learningScenario] = await db
+    .insert(sharedSchoolConversationTable)
+    .values({
+      name: '',
+      userId: generateUUID(),
+      modelId: generateUUID(),
+      ...data,
+    })
+    .returning();
+  if (!learningScenario) {
+    throw Error('failed to create learning scenario');
+  }
+
+  const fileId = await createFile();
+  await db
+    .insert(SharedSchoolConversationFileMapping)
+    .values({ sharedSchoolConversationId: learningScenario.id, fileId });
+
+  return learningScenario;
+}
+
+const characterInsertSchema = createInsertSchema(characterTable).omit({ accessLevel: true });
+async function createCharacter(data?: Partial<z.infer<typeof characterInsertSchema>>) {
+  const [character] = await db
+    .insert(characterTable)
+    .values({
+      name: '',
+      userId: generateUUID(),
+      description: '',
+      modelId: generateUUID(),
+      ...data,
+    })
+    .returning();
+  if (!character) {
+    throw Error('failed to create character');
+  }
+
+  const fileId = await createFile();
+  await db.insert(CharacterFileMapping).values({ characterId: character.id, fileId });
+
+  return character;
+}
+
+const customGptInsertSchema = createInsertSchema(customGptTable).omit({ accessLevel: true });
+async function createCustomGpt(data?: Partial<z.infer<typeof customGptInsertSchema>>) {
+  const [customGpt] = await db
+    .insert(customGptTable)
+    .values({
+      name: '',
+      systemPrompt: '',
+      userId: generateUUID(),
+      ...data,
+    })
+    .returning();
+  if (!customGpt) {
+    throw Error('failed to create custom GPT');
+  }
+
+  const fileId = await createFile();
+  await db.insert(CustomGptFileMapping).values({ customGptId: customGpt.id, fileId });
+
+  return customGpt;
+}
+
+async function createFile() {
+  const fileId = generateUUID();
+  await db.insert(fileTable).values({ id: fileId, name: '', size: 0, type: 'plain/text' });
+  return fileId;
+}
