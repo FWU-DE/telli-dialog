bd5ae06d0ca5cc9c3578f7fb9a7d40c681b576b4
Maximilian Asam <maximilian.asam@xitaso.com>
2025-12-12 11:05:03 +0100

    temporary loading animation instead of spinner

--- apps/dialog/src/components/image-generation/loading-animation.tsx
+++ apps/dialog/src/components/image-generation/loading-animation.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+import Image from 'next/image';
 
 interface LoadingAnimationProps {
   message?: string;
@@ -15,9 +16,15 @@ export default function LoadingAnimation({
     <div
       className={`flex flex-col items-center justify-center aspect-square border-2 border-none bg-gray-50 rounded-enterprise-md mb-4 ${className}`}
     >
-      {/* TODO: When Gif is supplied, replace with the following line */}
+      {/* TODO: When Gif is updated, replace with the following line */}
       {/* <Image src="/loading.gif" alt="Ladeanimation" width="107" height="107" unoptimized /> */}
-      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-primary mb-4"></div>
+      <Image
+        src="/loading-transparent.gif"
+        alt="Ladeanimation"
+        width="107"
+        height="107"
+        unoptimized
+      />
       <span className="text-gray-900 text-sm">{message}</span>
     </div>
   );
