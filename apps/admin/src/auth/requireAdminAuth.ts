1af82df81de493eba12e6f19decdf1605ad6e217
Tobias Wallura <tobias.wallura@xitaso.com>
2025-12-16 13:18:33 +0100

    TD-702 use UnauthenticatedError from shared package

--- apps/admin/src/auth/requireAdminAuth.ts
+++ apps/admin/src/auth/requireAdminAuth.ts
@@ -1,13 +1,6 @@
 import { auth } from '@/auth';
 import type { Session } from 'next-auth';
-
-// TODO: Replace with shared error class when available
-export class UnauthenticatedError extends Error {
-  constructor(message = 'Authentication required') {
-    super(message);
-    this.name = 'UnauthenticatedError';
-  }
-}
+import { UnauthenticatedError } from '@shared/error';
 
 // Type for a validated admin session with guaranteed user.name
 export type ValidatedSession = Session & {
