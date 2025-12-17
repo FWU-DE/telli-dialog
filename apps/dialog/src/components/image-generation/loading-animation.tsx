934765642e513bd63c6fb4ab6189581194c73d73
Maximilian Asam <maximilian.asam@xitaso.com>
2025-12-17 13:16:57 +0100

    new loading animation

--- apps/dialog/src/components/image-generation/loading-animation.tsx
+++ apps/dialog/src/components/image-generation/loading-animation.tsx
@@ -16,15 +16,7 @@ export default function LoadingAnimation({
     <div
       className={`flex flex-col items-center justify-center aspect-square border-2 border-none bg-gray-50 rounded-enterprise-md mb-4 ${className}`}
     >
-      {/* TODO: When Gif is updated, replace with the following line */}
-      {/* <Image src="/loading.gif" alt="Ladeanimation" width="107" height="107" unoptimized /> */}
-      <Image
-        src="/loading-transparent.gif"
-        alt="Ladeanimation"
-        width="107"
-        height="107"
-        unoptimized
-      />
+      <Image src="/loading.gif" alt="Ladeanimation" width="107" height="107" unoptimized />
       <span className="text-gray-900 text-sm">{message}</span>
     </div>
   );
