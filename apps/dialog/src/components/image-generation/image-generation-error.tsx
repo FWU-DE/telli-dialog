ed77683751ea746b00b44d51ec407b93543ccd5c
Maximilian Asam <maximilian.asam@xitaso.com>
2025-12-17 13:59:32 +0100

    New Error display component

---
+++ apps/dialog/src/components/image-generation/image-generation-error.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import React from 'react';
+import { WarningIcon } from '@phosphor-icons/react';
+
+interface ImageGenerationErrorProps {
+  message: string;
+}
+
+export function ImageGenerationError({ message }: ImageGenerationErrorProps) {
+  return (
+    <div className="mt-6 w-full">
+      <div className="border text-dark-red border-dark-red bg-light-red rounded-xl py-4 px-6 flex flex-row items-center gap-4">
+        <WarningIcon size={32} />
+        <p className="text-sm text-black" role="alert">
+          {message}
+        </p>
+      </div>
+    </div>
+  );
+}
