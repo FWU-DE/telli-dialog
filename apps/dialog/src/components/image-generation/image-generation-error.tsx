27e020eba39317f2f95cdb2e1d23d17a607a9f47
Maximilian Asam <maximilian.asam@xitaso.com>
2025-12-17 14:19:06 +0100

    pr comments

--- apps/dialog/src/components/image-generation/image-generation-error.tsx
+++ apps/dialog/src/components/image-generation/image-generation-error.tsx
@@ -11,7 +11,7 @@ export function ImageGenerationError({ message }: ImageGenerationErrorProps) {
   return (
     <div className="mt-6 w-full">
       <div className="border text-dark-red border-dark-red bg-light-red rounded-xl py-4 px-6 flex flex-row items-center gap-4">
-        <WarningIcon size={32} />
+        <WarningIcon size={32} aria-hidden="true" />
         <p className="text-sm text-black" role="alert">
           {message}
         </p>
