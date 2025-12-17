e20cc84fbcae8331fc4c447c4f8aed207de82cc3
Tobias Wallura <161311015+TobiasWallura-xitaso@users.noreply.github.com>
2025-12-17 11:05:37 +0100

    TD-728 prevent unresponsive valkey from taking down the whole application (#421)
    * TD-728 prevent unresponsive valkey from taking down the whole application
    
    * TD-728 remove obsolete code from vidis-mock
    
    * TD-728 move ioredis to shared package and update unstorage to fix typing issue

--- apps/dialog/src/auth/session.ts
+++ apps/dialog/src/auth/session.ts
@@ -1,13 +1,5 @@
-import { env } from '@/env';
-import { createStorage } from 'unstorage';
-import redisDriver from 'unstorage/drivers/redis';
-
-const storage = createStorage({
-  driver: redisDriver({
-    url: env.valkeyUrl,
-    base: 'telli:dialog',
-  }),
-});
+import { logError } from '@shared/logging';
+import { valkey } from '@shared/valkey';
 
 const SESSION_PREFIX = 'telli:dialog:session:';
 const TIME_TO_LIVE_SECONDS = 60 * 60 * 24; // remove blocklist items automatically after 24 hours
@@ -15,9 +7,14 @@ const TIME_TO_LIVE_SECONDS = 60 * 60 * 24; // remove blocklist items automatical
 /** List of outdated sessions because user logged out elsewhere. */
 export const sessionBlockList = {
   add: async (sessionId: string) => {
-    await storage.setItem(SESSION_PREFIX + sessionId, true, { ttl: TIME_TO_LIVE_SECONDS });
+    await valkey.setItem(SESSION_PREFIX + sessionId, true, { ttl: TIME_TO_LIVE_SECONDS });
   },
   has: async (sessionId: string) => {
-    return storage.hasItem(SESSION_PREFIX + sessionId);
+    try {
+      return await valkey.hasItem(SESSION_PREFIX + sessionId);
+    } catch (error) {
+      logError('Failed to check for session logout, allowing login', error);
+      return false;
+    }
   },
 };
