e6cee96354d783c7e86f8686304e64c1c16e7441
robin.laerm <robin.laerm@xitaso.com>
2026-01-09 12:16:56 +0100

    shared chats do not allow to change the model

--- apps/dialog/src/app/api/shared-chat/route.ts
+++ apps/dialog/src/app/api/shared-chat/route.ts
@@ -31,7 +31,7 @@ import {
 import { limitChatHistory } from '../chat/utils';
 
 export async function POST(request: NextRequest) {
-  const { messages, modelId }: { messages: Array<Message>; modelId: string } = await request.json();
+  const { messages }: { messages: Array<Message> } = await request.json();
 
   const { sharedChatId, inviteCode } = getSearchParamsOrThrow(request.url);
   const sharedChat = await dbGetSharedChatByIdAndInviteCode({ id: sharedChatId, inviteCode });
@@ -55,7 +55,7 @@ export async function POST(request: NextRequest) {
     );
   }
   const [error, modelAndProvider] = await getModelAndProviderWithResult({
-    modelId,
+    modelId: sharedChat.modelId,
     federalStateId: teacherUserAndContext.federalState.id,
   });
 
