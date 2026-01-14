65b8a646aa6670bd689ed6a8567aca2c48a15058
robin.laerm <robin.laerm@xitaso.com>
2026-01-14 11:27:34 +0100

    add text to german localization file

--- apps/dialog/src/app/(unauth)/login/error/page.tsx
+++ apps/dialog/src/app/(unauth)/login/error/page.tsx
@@ -1,6 +1,7 @@
 import LogoutButton from '@/app/(authed)/logout-button';
 import WarningIcon from '@/components/icons/warning-icon';
 import { getFieldErrorsFromUrl } from '@shared/auth/authentication-service';
+import { getTranslations } from 'next-intl/server';
 
 export const dynamic = 'force-dynamic';
 
@@ -12,15 +13,13 @@ const fieldNameMappings = {
 
 export default async function Page(props: PageProps<'/login/error'>) {
   const missingFieldsInProfile = getFieldErrorsFromUrl(await props.searchParams);
+  const t = await getTranslations('authentication.login-error');
 
   return (
     <div className="flex justify-center min-h-screen items-center">
       <div className="p-6 flex flex-col gap-4 items-center rounded-xl border bg-light-gray max-w-fit">
         <WarningIcon />
-        <div>
-          Login leider nicht möglich. Folgende Informationen müssen mit dem Profil übermittelt
-          werden:
-        </div>
+        <div>{t('description')}</div>
         <ul>
           {missingFieldsInProfile.map((field) => {
             return (
