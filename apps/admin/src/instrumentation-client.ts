eb0971cd70669a99570472319bf3fbb9496639f3
Tobias Wallura <161311015+TobiasWallura-xitaso@users.noreply.github.com>
2026-01-28 18:06:26 +0100

    TD-821 fix timing issue on client instrumentation setup

--- apps/admin/src/instrumentation-client.ts
+++ apps/admin/src/instrumentation-client.ts
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/nextjs';
 import { clientSentryInit } from '@shared/sentry/browser-init';
 
-clientSentryInit();
+void clientSentryInit();
 
 // This export will instrument router navigations, and is only relevant if you enable tracing.
 // `captureRouterTransitionStart` is available from SDK version 9.12.0 onwards
