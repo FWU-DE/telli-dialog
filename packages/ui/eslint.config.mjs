bd875479aee1f940336a76eb7e1084cfbcfd5ed8
Tobias Wallura <tobias.wallura@xitaso.com>
2026-02-03 14:50:39 +0100

    Fix check-types and lint for @telli/ui package

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -1,9 +1,13 @@
+import next from 'eslint-config-next';
 import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
 import nextTypescript from 'eslint-config-next/typescript';
+import turboConfig from 'eslint-config-turbo/flat';
 
 const eslintConfig = [
+  ...next,
   ...nextCoreWebVitals,
   ...nextTypescript,
+  ...turboConfig,
   {
     ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
   },
