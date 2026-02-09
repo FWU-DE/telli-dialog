b2ca3abee32567f70980de24ef9eeb69aa897e71
Tobias Wallura <tobias.wallura@xitaso.com>
2026-02-09 14:47:39 +0100

    TD-913 set schoolId on all learning scenarios where missing

--- packages/shared/migrations/0046_fat_reaper.sql
+++ packages/shared/migrations/0046_fat_reaper.sql
@@ -6,4 +6,18 @@ CREATE TABLE "learning_scenario_template_mappings" (
 --> statement-breakpoint
 ALTER TABLE "learning_scenario_template_mappings" ADD CONSTRAINT "learning_scenario_template_mappings_learning_scenario_id_fk" FOREIGN KEY ("learning_scenario_id") REFERENCES "public"."federal_state"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "learning_scenario_template_mappings" ADD CONSTRAINT "learning_scenario_template_mappings_federal_state_id_fk" FOREIGN KEY ("federal_state_id") REFERENCES "public"."federal_state"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "shared_learning_scenario" ADD CONSTRAINT "shared_learning_scenario_learning_scenario_id_fk" FOREIGN KEY ("learning_scenario_id") REFERENCES "public"."learning_scenario"("id") ON DELETE cascade ON UPDATE no action;
+ALTER TABLE "shared_learning_scenario" ADD CONSTRAINT "shared_learning_scenario_learning_scenario_id_fk" FOREIGN KEY ("learning_scenario_id") REFERENCES "public"."learning_scenario"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+
+-- Set the schoolId for all learning scenarios; required for school-internal sharing
+WITH first_school_per_user AS (
+    SELECT DISTINCT ON (usm.user_id)
+        usm.user_id,
+        usm.school_id
+    FROM user_school_mapping usm
+    ORDER BY usm.user_id, usm.created_at
+)
+UPDATE learning_scenario ls
+SET school_id = fs.school_id
+FROM first_school_per_user fs
+WHERE ls.school_id IS NULL
+  AND fs.user_id = ls.user_id;
