4af1829011529dee1daf50e047a016ebadebec5c
Anton Winschel <anton.winschel@xitaso.com>
2026-02-26 16:23:24 +0100

    T-966: fix migration

--- packages/shared/migrations/0051_foamy_the_call.sql
+++ packages/shared/migrations/0051_foamy_the_call.sql
@@ -1,2 +1,2 @@
-DROP INDEX "text_chunk_content_tsv_idx";--> statement-breakpoint
-ALTER TABLE "text_chunk" DROP COLUMN "content_tsv";
+DROP INDEX IF EXISTS "text_chunk_content_tsv_idx";--> statement-breakpoint
+ALTER TABLE "text_chunk" DROP COLUMN IF EXISTS "content_tsv";
