f2e8daf46d6439bc5452f8078cf74a7b97772964
Maximilian Asam <maximilian.asam@xitaso.com>
2026-02-27 15:09:53 +0100

    cleanup useless try catch

--- apps/dialog/src/auth/requireValidInviteCode.ts
+++ apps/dialog/src/auth/requireValidInviteCode.ts
@@ -4,10 +4,6 @@ import {
 } from '@shared/sharing/get-chat-info-by-invite-code';
 
 export async function requireValidInviteCode(inviteCode: string): Promise<{ chatInfo: ChatInfo }> {
-  try {
     const chatInfo = await getChatInfoByInviteCode(inviteCode);
     return { chatInfo };
-  } catch (error) {
-    throw error;
-  }
 }
