0b0eb40a9a08e089ae6f1cfcc1f3c0a5fc723237
robin.laerm <robin.laerm@xitaso.com>
2026-02-28 16:38:16 +0100

    use configuration from packages/ui

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import tailwindcssAnimate from 'tailwindcss-animate';
 
 const config = {
   content: [
@@ -6,21 +7,52 @@ const config = {
     './app/**/*.{ts,tsx}',
     '../../packages/ui/src/components/**/*.{ts,tsx}',
   ],
+  prefix: '',
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
     extend: {
+      screens: {
+        xs: '480px',
+      },
+      listStyleType: {
+        square: 'square',
+        roman: 'upper-roman',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
       colors: {
+        'main-black': 'rgba(0, 0, 0, 1)',
+        'dark-gray': '#333333',
+        'light-gray': '#f5f5f5',
+        'semilight-gray': '#fafafa',
+        coral: '#e94d52',
+        'dark-red': '#9f070d',
+        'light-red': '#fef6f6',
+        'primary-hover': 'var(--primary-hover)',
+        'primary-hover-text': 'var(--primary-hover-text)',
+        'secondary-hover': 'var(--secondary-hover)',
+        'secondary-light': 'var(--secondary-light)',
+        'button-primary-text': 'var(--button-primary-text)',
+        'primary-text': 'var(--primary-text)',
+        'secondary-text': 'var(--secondary-text)',
+        'secondary-dark': 'var(--secondary-dark)',
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
         primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
+          DEFAULT: 'var(--primary)',
+          foreground: 'var(--primary-text)',
         },
         secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
+          DEFAULT: 'var(--secondary)',
+          foreground: 'var(--secondary-text)',
         },
         destructive: {
           DEFAULT: 'hsl(var(--destructive))',
@@ -44,9 +76,13 @@ const config = {
         },
       },
       borderRadius: {
+        '1': '1px',
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
+        'enterprise-full': '999px',
+        'enterprise-md': '12px',
+        'enterprise-sm': '6px',
       },
       keyframes: {
         'accordion-down': {
@@ -57,14 +93,31 @@ const config = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        hide: {
+          from: { opacity: '1' },
+          to: { opacity: '0' },
+        },
+        slideIn: {
+          from: {
+            transform: 'translateX(calc(100% + var(--viewport-padding)))',
+          },
+          to: { transform: 'translateX(0)' },
+        },
+        swipeOut: {
+          from: { transform: 'translateX(var(--radix-toast-swipe-end-x))' },
+          to: { transform: 'translateX(calc(100% + var(--viewport-padding)))' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        hide: 'hide 100ms ease-in',
+        slideIn: 'slideIn 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        swipeOut: 'swipeOut 100ms ease-out',
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindcssAnimate],
 } satisfies Config;
 
 export default config;
