037b04b437d779d39b5fb303c538d201d99940c8
Anton Winschel <anton.winschel@xitaso.com>
2026-03-03 00:40:38 +0100

    fix migration

--- packages/shared/migrations/0053_familiar_mandroid.sql
+++ packages/shared/migrations/0053_familiar_mandroid.sql
@@ -1,9 +1,9 @@
 ALTER TABLE "text_chunk" RENAME TO "chunk";--> statement-breakpoint
 ALTER TABLE "chunk" DROP CONSTRAINT "text_chunk_file_id_file_table_id_fk";
 --> statement-breakpoint
-DROP INDEX "text_chunk_file_id_index";--> statement-breakpoint
-DROP INDEX "text_chunk_embedding_idx";--> statement-breakpoint
-DROP INDEX "text_chunk_source_url_index";--> statement-breakpoint
+DROP INDEX IF EXISTS "text_chunk_file_id_index";--> statement-breakpoint
+DROP INDEX IF EXISTS "text_chunk_embedding_idx";--> statement-breakpoint
+DROP INDEX IF EXISTS "text_chunk_source_url_index";--> statement-breakpoint
 ALTER TABLE "chunk" ADD CONSTRAINT "chunk_file_id_file_table_id_fk" FOREIGN KEY ("file_id") REFERENCES "public"."file_table"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 CREATE INDEX "chunk_file_id_index" ON "chunk" USING btree ("file_id");--> statement-breakpoint
 CREATE INDEX "chunk_embedding_idx" ON "chunk" USING hnsw ("embedding" vector_cosine_ops);--> statement-breakpoint
