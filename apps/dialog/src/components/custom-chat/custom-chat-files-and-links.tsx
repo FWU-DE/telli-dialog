fc0a34ea137e41c39ffb0673963e1cabca20dace
robin.laerm <robin.laerm@xitaso.com>
2026-03-23 11:03:49 +0100

    Tooltip updated

--- apps/dialog/src/components/custom-chat/custom-chat-files-and-links.tsx
+++ apps/dialog/src/components/custom-chat/custom-chat-files-and-links.tsx
@@ -2,13 +2,19 @@ import { Card, CardContent, CardHeader, CardTitle } from '@ui/components/Card';
 import { CustomChatFiles, CustomChatFilesProps } from './custom-chat-files';
 import { CustomChatHeading2 } from './custom-chat-heading2';
 import { CustomChatLinks, CustomChatLinksProps } from './custom-chat-links';
+import { useTranslations } from 'next-intl';
 
 type CustomChatFilesAndLinksProps = CustomChatFilesProps & CustomChatLinksProps;
 
 export function CustomChatFilesAndLinks(props: CustomChatFilesAndLinksProps) {
+  const t = useTranslations('custom-gpt.form');
+
   return (
     <div className="flex flex-col gap-3 mt-10">
-      <CustomChatHeading2 text="Hintergrundwissen" tooltip="Platzhaltertext" />
+      <CustomChatHeading2
+        text={t('additional-assets-label')}
+        tooltip={t('additional-assets-content')}
+      />
       <Card>
         <CardHeader>
           <CardTitle>Dateien</CardTitle>
