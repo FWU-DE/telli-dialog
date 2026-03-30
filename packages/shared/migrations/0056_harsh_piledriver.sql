b66703d19d8ea21d07994d477ca6b139fa558640
Maximilian Asam <maximilian.asam@xitaso.com>
2026-03-30 14:38:24 +0200

    statement-breakpoint

--- packages/shared/migrations/0056_harsh_piledriver.sql
+++ packages/shared/migrations/0056_harsh_piledriver.sql
@@ -1,7 +1,7 @@
 ALTER TABLE "character" ADD COLUMN "updated_at" timestamp with time zone DEFAULT now() NOT NULL;--> statement-breakpoint
-ALTER TABLE "learning_scenario" ADD COLUMN "updated_at" timestamp with time zone DEFAULT now() NOT NULL;
+ALTER TABLE "learning_scenario" ADD COLUMN "updated_at" timestamp with time zone DEFAULT now() NOT NULL;--> statement-breakpoint
 
-UPDATE "character" SET "updated_at" = "created_at";
-UPDATE "learning_scenario" SET "updated_at" = "created_at";
+UPDATE "character" SET "updated_at" = "created_at";--> statement-breakpoint
+UPDATE "learning_scenario" SET "updated_at" = "created_at";--> statement-breakpoint
 /* Since this wasn't done previously and both of the migrations will be deployed shortly after each other */
-UPDATE "assistant" SET "updated_at" = "created_at";
+UPDATE "assistant" SET "updated_at" = "created_at";--> statement-breakpoint
