debcd5d0fed580eac87dba942bb122492cafda68
Tobias Wallura <161311015+TobiasWallura-xitaso@users.noreply.github.com>
2026-04-01 13:36:34 +0200

    Fix usage of Logo component (#730)

--- apps/dialog/src/app/(authed)/(dialog)/assistants/d/[assistantId]/page.tsx
+++ apps/dialog/src/app/(authed)/(dialog)/assistants/d/[assistantId]/page.tsx
@@ -25,7 +25,7 @@ export default async function Page(props: PageProps<'/assistants/d/[assistantId]
     schoolId: school.id,
   }).catch(handleErrorInServerComponent);
 
-  const logoElement = <Logo federalStateId={federalState.id} />;
+  const logoElement = <Logo logoPath={federalState.pictureUrls?.logo} />;
   const models = await dbGetLlmModelsByFederalStateId({
     federalStateId: federalState.id,
   });
