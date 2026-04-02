d06a309f2c5b0443365999be08f0512f330e38ba
brueckb <bastian.brueck@xitaso.com>
2026-04-02 10:20:53 +0200

    add tooltip for websites

--- apps/dialog/src/components/custom-chat/custom-chat-files-and-links.tsx
+++ apps/dialog/src/components/custom-chat/custom-chat-files-and-links.tsx
@@ -1,4 +1,6 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@ui/components/Card';
+import { InfoIcon } from '@phosphor-icons/react';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@ui/components/Tooltip';
 import { CustomChatFiles, CustomChatFilesProps } from './custom-chat-files';
 import { CustomChatHeading2 } from './custom-chat-heading2';
 import { CustomChatLinks, CustomChatLinksProps } from './custom-chat-links';
@@ -27,7 +29,17 @@ export function CustomChatFilesAndLinks(props: CustomChatFilesAndLinksProps) {
 
       <Card>
         <CardHeader>
-          <CardTitle>{t('custom-assets-links')}</CardTitle>
+          <CardTitle>
+            <div className="flex items-center gap-1">
+              <span>{t('custom-assets-links')}</span>
+              <Tooltip>
+                <TooltipTrigger aria-label={t('custom-assets-links')}>
+                  <InfoIcon className="size-5 text-icon" />
+                </TooltipTrigger>
+                <TooltipContent>{t('custom-assets-links-content')}</TooltipContent>
+              </Tooltip>
+            </div>
+          </CardTitle>
         </CardHeader>
         <CardContent>
           <CustomChatLinks initialLinks={props.initialLinks} onLinksChange={props.onLinksChange} />
