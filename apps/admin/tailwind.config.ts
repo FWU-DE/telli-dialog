2ccc86820f72d22dfd82627357dcc1e37c2ce5ab
robin.laerm <robin.laerm@xitaso.com>
2026-04-08 08:12:12 +0200

    replace tailwind.config with globals.css

--- apps/admin/tailwind.config.ts
+++ apps/admin/tailwind.config.ts
@@ -1,9 +1,5 @@
 import type { Config } from 'tailwindcss';
-import baseConfig from '@telli/ui/tailwind.config';
 
-const config = {
-  ...baseConfig,
-  content: ['./src/app/**/*.{ts,tsx}', './src/components/**/*.{ts,tsx}'],
-} satisfies Config;
+const config = {} satisfies Config;
 
 export default config;
