2731d9bb15e21d6d2dd80fb19cf1fcef5b0e683e
Tobias Wallura <tobias.wallura@xitaso.com>
2026-04-13 15:23:24 +0200

    TD-1084 refactor expired-chat and product-access dialogs to use AlertDialog component

--- apps/dialog/src/components/modals/product-access.tsx
+++ apps/dialog/src/components/modals/product-access.tsx
@@ -1,11 +1,17 @@
 'use client';
 
 import React from 'react';
-import * as AlertDialog from '@radix-ui/react-alert-dialog';
-import Link from 'next/link';
 import { useTranslations } from 'next-intl';
-import { buttonSecondaryClassName } from '@/utils/tailwind/button';
-import { usePortalContainer } from '@ui/components/portal-container';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@ui/components/AlertDialog';
+import { useRouter } from 'next/navigation';
 
 const LOGOUT_URL = '/api/auth/logout';
 
@@ -14,32 +20,27 @@ type ProductAccessModalProps = {
   children: React.ReactNode;
 } & React.ComponentProps<'button'>;
 
-export default function ProductAccessModal({
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  onClick,
-  children,
-  modalTitle,
-}: ProductAccessModalProps) {
-  const container = usePortalContainer();
+export default function ProductAccessModal({ children, modalTitle }: ProductAccessModalProps) {
   const t = useTranslations('common');
+  const router = useRouter();
   return (
-    <AlertDialog.Root open defaultOpen>
-      <AlertDialog.Portal container={container}>
-        <AlertDialog.Overlay className="fixed inset-0 bg-[#333333] z-30 opacity-30 shadow-[0px_0px_80px_0px_rgba(0,41,102,0.1)]" />
-        <AlertDialog.Content className="z-50 fixed left-1/2 top-1/2 max-h-[85vh] -translate-x-1/2 -translate-y-1/2 rounded-enterprise-md bg-white p-10 w-[350px] lg:w-[564px] max-w-xl">
-          <AlertDialog.Title asChild>
-            <h1 className="text-3xl font-medium mb-6">{modalTitle}</h1>
-          </AlertDialog.Title>
-          <AlertDialog.Description asChild>{children}</AlertDialog.Description>
-          <div className="flex flex-wrap justify-end items-center gap-6 mt-10">
-            <AlertDialog.Action asChild>
-              <Link href={LOGOUT_URL} prefetch={false} className={buttonSecondaryClassName}>
-                {t('logout')}
-              </Link>
-            </AlertDialog.Action>
-          </div>
-        </AlertDialog.Content>
-      </AlertDialog.Portal>
-    </AlertDialog.Root>
+    <AlertDialog open defaultOpen>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{modalTitle}</AlertDialogTitle>
+        </AlertDialogHeader>
+        <AlertDialogDescription>{children}</AlertDialogDescription>
+        <AlertDialogFooter>
+          <AlertDialogAction
+            variant="outline"
+            onClick={() => {
+              router.push(LOGOUT_URL);
+            }}
+          >
+            {t('logout')}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
