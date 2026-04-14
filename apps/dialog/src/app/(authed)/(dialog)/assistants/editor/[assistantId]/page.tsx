99bedc62c96bae258e2ba3feb4b5788741697adf
robin.laerm <robin.laerm@xitaso.com>
2026-04-14 12:26:41 +0200

    adapt spacing in main layout

--- apps/dialog/src/app/(authed)/(dialog)/assistants/editor/[assistantId]/page.tsx
+++ apps/dialog/src/app/(authed)/(dialog)/assistants/editor/[assistantId]/page.tsx
@@ -4,12 +4,17 @@ import { getAssistantByUser } from '@shared/assistants/assistant-service';
 import { notFound } from 'next/navigation';
 import { AssistantEdit } from './assistant-edit';
 import { ResponsiveLayoutWrapper } from '../../../_components/responsive-layout-wrapper';
+import HeaderPortal from '../../../header-portal';
+import { ToggleSidebarButton } from '@/components/navigation/sidebar/collapsible-sidebar';
+import ProfileMenu from '@/components/navigation/profile-menu';
+import { buildLegacyUserAndContext } from '@/auth/types';
 
 export const dynamic = 'force-dynamic';
 
 export default async function Page(props: PageProps<'/assistants/editor/[assistantId]'>) {
   const { assistantId } = await props.params;
   const { user, school, federalState } = await requireAuth();
+  const userAndContext = buildLegacyUserAndContext(user, school, federalState);
 
   if (!federalState.featureToggles.isNewUiDesignEnabled) {
     notFound();
@@ -27,6 +32,13 @@ export default async function Page(props: PageProps<'/assistants/editor/[assista
 
   return (
     <ResponsiveLayoutWrapper>
+      <HeaderPortal>
+        <ToggleSidebarButton
+          isNewUiDesignEnabled={federalState.featureToggles.isNewUiDesignEnabled}
+        />
+        <div className="grow"></div>
+        <ProfileMenu userAndContext={userAndContext} />
+      </HeaderPortal>
       <AssistantEdit
         assistant={assistant}
         relatedFiles={fileMappings}
