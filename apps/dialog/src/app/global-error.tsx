7943946a1e0268090d9719816d0791a263f9bb16
Tobias Wallura <tobias.wallura@xitaso.com>
2026-04-15 18:03:49 +0200

    TD-1110 fix a11y lint issues

--- apps/dialog/src/app/global-error.tsx
+++ apps/dialog/src/app/global-error.tsx
@@ -13,27 +13,29 @@ export default function GlobalError({ reset, error }: { error: Error; reset: () 
   }, [error]);
 
   return (
-    <html>
-      <div className="w-screen h-screen flex flex-col justify-center items-center">
-        <h1 className="text-[10rem] font-bold">
-          <span className="text-main-black">Fe</span>
-          <span className="text-main-red">hl</span>
-          <span className="text-main-gold">er</span>
-        </h1>
-        <h2 className="text-xl font-bold">Es ist leider ein Fehler aufgetreten.</h2>
-        <button
-          className={'btn-primary mt-12'}
-          onClick={
-            // Attempt to recover by trying to re-render the segment
-            () => {
-              router.refresh();
-              reset();
+    <html lang="de">
+      <body>
+        <div className="w-screen h-screen flex flex-col justify-center items-center">
+          <h1 className="text-[10rem] font-bold">
+            <span className="text-main-black">Fe</span>
+            <span className="text-main-red">hl</span>
+            <span className="text-main-gold">er</span>
+          </h1>
+          <h2 className="text-xl font-bold">Es ist leider ein Fehler aufgetreten.</h2>
+          <button
+            className="btn-primary mt-12"
+            onClick={
+              // Attempt to recover by trying to re-render the segment
+              () => {
+                router.refresh();
+                reset();
+              }
             }
-          }
-        >
-          Erneut versuchen
-        </button>
-      </div>
+          >
+            Erneut versuchen
+          </button>
+        </div>
+      </body>
     </html>
   );
 }
