6bc5a8efad1631f462a82b6a683fa8d923daf6e8
Maximilian Asam <maximilian.asam@xitaso.com>
2026-04-15 14:24:06 +0200

    Another one was gray

--- apps/dialog/src/components/entity-overview/entity-overview.tsx
+++ apps/dialog/src/components/entity-overview/entity-overview.tsx
@@ -156,7 +156,7 @@ export default function EntityOverview({
           </div>
         </div>
 
-        <div className="px-6 py-2 pb-4 sticky top-0 z-10 bg-gray-50">
+        <div className="px-6 py-2 pb-4 sticky top-0 z-10">
           <div className="max-w-3xl mx-auto w-full">
             <div className="flex items-end flex-wrap gap-2" aria-label={t('filter-tabs-label')}>
               <FilterTabs
