7ea6abd39d8a25c88c9441705e4ab74426a75687
Tobias Wallura <tobias.wallura@xitaso.com>
2026-04-22 00:18:16 +0200

    TD-1112 use 4 workers for parallel e2e tests

--- apps/dialog/playwright.config.ts
+++ apps/dialog/playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
-  workers: process.env.CI ? 3 : 1,
+  workers: process.env.CI ? 4 : 1,
   reporter: [['html', { outputFolder: './playwright-report' }], ['json'], ['github'], ['list']],
   timeout: 90_000,
   use: {
