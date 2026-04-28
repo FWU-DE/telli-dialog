317e275aec96a2ffd078ec594fe2cb5a14b69fbe
Tobias Wallura <tobias.wallura@xitaso.com>
2026-04-28 12:46:09 +0200

    TD-1047: recreate db migration

--- packages/shared/migrations/0063_nosy_korath.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "shared_character_conversation" DROP CONSTRAINT "shared_character_conversation_character_id_user_id_unique";--> statement-breakpoint
-ALTER TABLE "shared_learning_scenario" DROP CONSTRAINT "shared_learning_scenario_learning_scenario_id_user_id_unique";--> statement-breakpoint
-ALTER TABLE "shared_character_conversation" ADD COLUMN "manually_stopped_at" timestamp with time zone;--> statement-breakpoint
-ALTER TABLE "shared_learning_scenario" ADD COLUMN "manually_stopped_at" timestamp with time zone;
