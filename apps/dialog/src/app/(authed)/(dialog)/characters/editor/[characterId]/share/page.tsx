f01e28c6fa783c266ab387581b33789e7e490b86
Tobias Wallura <tobias.wallura@xitaso.com>
2026-05-05 16:24:07 +0200

    TD-1166: remove unnecessary fallback

--- apps/dialog/src/app/(authed)/(dialog)/characters/editor/[characterId]/share/page.tsx
+++ apps/dialog/src/app/(authed)/(dialog)/characters/editor/[characterId]/share/page.tsx
@@ -47,7 +47,7 @@ export default async function Page(props: PageProps<'/characters/editor/[charact
         <h1 className="text-4xl sm:text-5xl font-medium mb-10">{t('join')}</h1>
         <CountDownTimer
           leftTimeInSeconds={leftTime}
-          totalTimeInMinutes={character.maxUsageTimeLimit ?? 0}
+          totalTimeInMinutes={character.maxUsageTimeLimit}
           stopWatchClassName="w-4 h-4"
         />
         <main className="grid grid-cols-1 sm:grid-cols-[1fr_auto_1fr] w-full gap-6 mt-6 sm:mt-8 mb-12 sm:mb-16">
