8368a4992305fba9d37aab6beaaf38ffc2001171
robin.laerm <robin.laerm@xitaso.com>
2026-07-21 16:57:31 +0200

    add translation text for not found

--- apps/chat-bot/src/components/shared-chat/shared-chat-not-found.tsx
+++ apps/chat-bot/src/components/shared-chat/shared-chat-not-found.tsx
@@ -2,15 +2,18 @@
 
 import CollapseSidebar from '@/components/common/collapse-sidebar';
 import { WarningIcon } from '@phosphor-icons/react';
+import { useTranslations } from 'next-intl';
 
 export default function SharedChatNotFound() {
+  const t = useTranslations('common');
+
   return (
     <>
       <CollapseSidebar />
       <div className="flex justify-center">
         <div className="p-6 flex flex-col gap-4 items-center rounded-xl border bg-light-gray max-w-fit">
           <WarningIcon className="size-12 text-primary" aria-hidden="true" />
-          <span>Diese Ressource existiert nicht oder ist nicht freigegeben.</span>
+          <span>{t('not-found-error')}</span>
         </div>
       </div>
     </>
